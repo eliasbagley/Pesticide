e1082f4966f698d59a8d4ec952a7740faadfdbdb
Elias Bagley <elias@rocketmade.com>
2014-11-22 03:00:31 -0700

    Add crosshair view

--- debugdrawer/Pesticide.swift
+++ debugdrawer/Pesticide.swift
@@ -22,6 +22,7 @@ public class Pesticide {
         static var window = UIWindow()
         static var isSetup = false
         static var viewInspector: ViewInspector?
+        static var crosshairOverlay: CrosshairOverlay?
     }
 
     public class func log(message: String) {
@@ -128,6 +129,15 @@ public class Pesticide {
                 }
             } else {
                 CV.viewInspector?.done()
+            }
+        })
+        Pesticide.addSwitch(false, name:"Crosshair View", block: { (on: Bool) in
+            if (on) {
+                CV.crosshairOverlay = CrosshairOverlay(frame: CV.window.bounds)
+                CV.window.rootViewController?.view.addSubview(CV.crosshairOverlay!)
+                self.toggle()
+            } else {
+                CV.crosshairOverlay?.removeFromSuperview()
             }
         })
 
