2a128788e450145fadd4e28ad49f530a9bcd7a09
Elias Bagley <elias@rocketmade.com>
2014-11-21 19:38:28 -0700

    Add Build utils

--- debugdrawer/AppDelegate.swift
+++ debugdrawer/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         let threeFingerTap = UITapGestureRecognizer(target: self, action: Selector("threeFingerTap"))
         threeFingerTap.numberOfTouchesRequired = 3
         self.window?.addGestureRecognizer(threeFingerTap)
