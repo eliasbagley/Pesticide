287ba176f24c9449c45a78677d21e322db47a740
Daniel Gubler <danielcgubler@gmail.com>
2014-11-22 02:29:00 -0700

    Add hidden view

--- debugdrawer/SampleViewController.swift
+++ debugdrawer/SampleViewController.swift
@@ -8,9 +8,10 @@
 
 class SampleViewController : UIViewController {
     
-    let textField : UITextField = UITextField()
-    let enterButton : UIButton = UIButton()
-    let label : UILabel = UILabel()
+    let textField: UITextField = UITextField()
+    let enterButton: UIButton = UIButton()
+    let label: UILabel = UILabel()
+    let coolLabel: UILabel = UILabel()
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -62,10 +63,14 @@ class SampleViewController : UIViewController {
         self.enterButton.setTitle("Say Hello", forState: .Normal)
         self.enterButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.textField.placeholder = "Your Name"
+        self.coolLabel.text = "COOL LABEL"
+        self.coolLabel.backgroundColor = .yellowColor()
+        self.coolLabel.textColor = .blackColor()
         
         self.textField.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.enterButton.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.coolLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         self.textField.backgroundColor = .whiteColor()
         self.enterButton.backgroundColor = .whiteColor()
@@ -74,6 +79,7 @@ class SampleViewController : UIViewController {
         self.view.addSubview(self.textField)
         self.view.addSubview(self.enterButton)
         self.view.addSubview(self.label)
+        self.view.addSubview(self.coolLabel)
         
         self.applyConstraints()
     }
@@ -82,10 +88,14 @@ class SampleViewController : UIViewController {
         
         let bindings = ["textField": self.textField,
                         "button": self.enterButton,
-                        "label": self.label]
+                        "label": self.label,
+                        "coolLabel": self.coolLabel]
         
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "V:|-(40)-[textField(40)]-[button(40)]-[label(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: bindings))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-(40)-[coolLabel(40)]", options: NSLayoutFormatOptions(0), metrics: nil, views: bindings))
         
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "|-[textField]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: bindings))
@@ -95,5 +105,7 @@ class SampleViewController : UIViewController {
 
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
             "|-[label]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: bindings))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-[coolLabel]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: bindings))
     }
 }
