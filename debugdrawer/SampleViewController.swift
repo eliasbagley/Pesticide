f2c570bb58cfea0448535700cd1c6e27a3241acf
Elias Bagley <elias@rocketmade.com>
2014-11-22 05:45:01 -0700

    Update label messaging

--- debugdrawer/SampleViewController.swift
+++ debugdrawer/SampleViewController.swift
@@ -87,7 +87,7 @@ class SampleViewController : UIViewController {
         self.enterButton.setTitle("Say Hello", forState: .Normal)
         self.enterButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.textField.placeholder = "Your Name"
-        self.coolLabel.text = " This label shouldn't be here"
+        self.coolLabel.text = " This label is in the way"
         self.coolLabel.backgroundColor = .yellowColor()
         self.coolLabel.textColor = .blackColor()
         
